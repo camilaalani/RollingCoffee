ec8f60f24cd5610d9501e9ade75abf1c5eb76495
Camila Alani <alanicamila124@gmail.com>
2024-03-24 16:22:48 -0300

    agregue el maquetado de la pagina del administrador

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <>
       <Menu></Menu>
       {/*<Inicio></Inicio>*/}
-      {/* <Administrador></Administrador> */}
-      <Error404></Error404>
+      <Administrador></Administrador>
+      {/*<Error404></Error404>*/}
       <Footer></Footer>
     </>
   )
