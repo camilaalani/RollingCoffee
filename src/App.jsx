f397861ed5caacba53622d3773d439bf70230338
Camila Alani <alanicamila124@gmail.com>
2024-03-23 23:45:30 -0300

    agregue el footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/common/Footer'
 import Menu from './components/common/Menu'
 import Inicio from './components/pages/Inicio'
+import Administrador from './components/pages/Administrador';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
       <Menu></Menu>
       <Inicio></Inicio>
+      {/* <Administrador></Administrador> */}
       <Footer></Footer>
     </>
   )
